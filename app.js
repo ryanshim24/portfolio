bfaf7b4dec130fd181bf02f7d1b0c5b7f42dd12b
ryanshim24 <ryanshim24@yahoo.com>
2014-11-07 23:23:59 -0800

    baby

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+    app = express(),
+    request = require("request"),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override');
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/assets'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.get('/about', function(req,res) { 
+  res.render('about');
+});
+
+var server = app.listen(process.env.PORT || 3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
